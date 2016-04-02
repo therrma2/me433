c5c329bf1ab17d2a4c6809ce632f4942954aea1c
Tim Herrmann <therrma2@gmail.com>
2016-04-02 13:33:23 -0500

    added #pragma commands for device config

---
+++ HW1.X/main.c
@@ -0,0 +1,25 @@
+#pragma config FUSBIDIO = ON
+#pragma config FVBUSONIO = ON
+#pragma config FPLLIDIV = DIV_2 //not sure on this one yet
+#pragma config FPLLMUL = MUL_20 //not sure
+#pragma config UPLLIDIV = DIV_2 //not sure
+#pragma config UPLLEN = ON
+#pragma config FPLLODIV = DIV_1 //not sure
+#pragma config FNOSC = PRIPLL
+#pragma config FSOSCEN = OFF
+#pragma config POSCMOD = HS
+#pragma config FPBDIV = DIV_1
+#pragma config WDTPS = PS4096
+#pragma config FWDTEN = OFF
+#pragma config DEBUG = OFF
+#pragma config ICESEL = ICS_PGx1  //different from NU32
+#pragma config BWP = ON
+
+
+#include <xc.h>
+
+void main(){
+    while(1){
+        
+    }
+}
