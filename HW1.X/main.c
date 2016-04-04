8add99ce11c3c89fbffd6b435286d60eada0dbb2
Tim Herrmann <therrma2@gmail.com>
2016-04-04 13:42:58 -0500

    Fixed while loop.  Program now outputs a 100KHz square wave on pin RB2 (pin 6)

--- HW1.X/main.c
+++ HW1.X/main.c
@@ -15,10 +15,10 @@
 #pragma config FSOSCEN = OFF // turn off secondary oscillator
 #pragma config IESO = OFF // no switching clocks
 #pragma config POSCMOD = HS // high speed crystal mode
-#pragma config OSCIOFNC = OFF // free up secondary osc pins  NOT SURE ON NTHIS ONE
+#pragma config OSCIOFNC = OFF // free    up secondary osc pins  NOT SURE ON NTHIS ONE
 #pragma config FPBDIV = DIV_1 // divide CPU freq by 1 for peripheral bus clock
 #pragma config FCKSM = CSDCMD // do not enable clock switch
-#pragma config WDTPS = 1048576 // slowest wdt
+#pragma config WDTPS = PS1048576 // slowest wdt
 #pragma config WINDIS = OFF // no wdt window
 #pragma config FWDTEN = OFF // wdt off by default
 #pragma config FWDTWINSZ = WINSZ_25 // wdt window at 25%
@@ -31,7 +31,7 @@
 #pragma config UPLLEN = ON // USB clock on
 
 // DEVCFG3
-#pragma config USERID = 0 // some 16bit userid, doesn't matter what
+#pragma config USERID = 13 // some 16bit userid, doesn't matter what
 #pragma config PMDL1WAY = OFF // allow multiple reconfigurations
 #pragma config IOL1WAY = OFF // allow multiple reconfigurations
 #pragma config FUSBIDIO = ON // USB pins controlled by USB module
@@ -55,7 +55,8 @@ int main() {
     
     // do your TRIS and LAT commands here
     TRISBbits.TRISB2 = 0;
-
+    ANSELB = 0b011;
+    LATBbits.LATB2 = 0;
 
 
     
@@ -66,9 +67,10 @@ int main() {
 	    // use _CP0_SET_COUNT(0) and _CP0_GET_COUNT() to test the PIC timing
 		// remember the core timer runs at half the CPU speed
 
-      if(_CP0_GET_COUNT()>=2400){
-	LATBINV = 0b10;
-	_CP0_SET_COUNT(0);
+      if(_CP0_GET_COUNT()>=12000){
+        LATBINV = 0b100;
+        _CP0_SET_COUNT(0);
+      }
     }
     
     
