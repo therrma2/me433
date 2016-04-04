694c65f6297a6a0caeb2a0d34032967e1efd8ae3
Tim Herrmann <therrma2@gmail.com>
2016-04-04 14:22:48 -0500

    Switched output pin to RA4 and added input pin to RB4

--- HW1.X/main.c
+++ HW1.X/main.c
@@ -54,9 +54,9 @@ int main() {
     DDPCONbits.JTAGEN = 0;
     
     // do your TRIS and LAT commands here
-    TRISBbits.TRISB2 = 0;
-    ANSELB = 0b011;
-    LATBbits.LATB2 = 0;
+    TRISAbits.TRISA4 = 0; // Set RA4 to output (green LED)
+    TRISBbits.TRISB4 = 1; // Set RB4 to input (user button)
+    LATAbits.LATA2 = 0; //Set green LED to 1 to start 
 
 
     
@@ -67,7 +67,7 @@ int main() {
 	    // use _CP0_SET_COUNT(0) and _CP0_GET_COUNT() to test the PIC timing
 		// remember the core timer runs at half the CPU speed
 
-      if(_CP0_GET_COUNT()>=12000){
+      if(_CP0_GET_COUNT()>=24000){
         LATBINV = 0b100;
         _CP0_SET_COUNT(0);
       }
